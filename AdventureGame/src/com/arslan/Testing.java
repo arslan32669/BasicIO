4207592aeeaa61c3a759c7340d8f3280ab6021d0
arslan32669 <66172625+arslan32669@users.noreply.github.com>
2020-09-24 18:54:54 +0500

    AdventureGame

---
+++ AdventureGame/src/com/arslan/Testing.java
@@ -0,0 +1,10 @@
+package com.arslan;
+
+public class Testing {
+    public static void main(String[] args) {
+        String[] me = "my name is arslan".split(" ");
+        for (String i: me) {
+            System.out.println(i);
+        }
+    }
+}
